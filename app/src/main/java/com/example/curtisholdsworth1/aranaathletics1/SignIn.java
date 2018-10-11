ee65111610c49e9da8a76cffc7eeff2564d72bdb
Curtis Holdsworth <n8588244@qut.edu.au>
2018-10-11 18:47:49 +1000

    Initial commit

---
+++ app/src/main/java/com/example/curtisholdsworth1/aranaathletics1/SignIn.java
@@ -0,0 +1,13 @@
+package com.example.curtisholdsworth1.aranaathletics1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
