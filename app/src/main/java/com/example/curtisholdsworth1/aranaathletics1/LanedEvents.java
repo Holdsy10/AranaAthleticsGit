25797bfc1d027076c8374158eba87bd1f1e45b18
Curtis Holdsworth <n8588244@qut.edu.au>
2019-03-16 03:41:28 +1000

    Keypad Update
    Keypad fully working with only a few input options to be completed

--- app/src/main/java/com/example/curtisholdsworth1/aranaathletics1/LanedEvents.java
+++ app/src/main/java/com/example/curtisholdsworth1/aranaathletics1/LanedEvents.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.regex.Pattern;
 
 
 public class LanedEvents extends AppCompatActivity {
@@ -135,7 +136,9 @@ public class LanedEvents extends AppCompatActivity {
 
 
 
-    leftTextEntry.addTextChangedListener(new TextWatcher() {
+
+
+/*    leftTextEntry.addTextChangedListener(new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -144,42 +147,129 @@ public class LanedEvents extends AppCompatActivity {
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             String strleftTextEntry = leftTextEntry.getText().toString();
-            leftAthlete.setText(strleftTextEntry);
+            //leftAthlete.setText(strleftTextEntry);
 
-            if (strleftTextEntry == "0"){
-                leftTextEntry.setText(leftTextEntry.getText().toString() + "Zero");
 
-            }
         }
 
         @Override
         public void afterTextChanged(Editable s) {
 
+            String input = s.toString();
+            leftAthlete.setText(input);
+
+            if (input == "0") {
+                leftAthlete.setText("Zero");
+                s.replace(0, s.length(), "Zero");
+            }
         }
-    });
-    }
+    });*/
 
+    leftTextEntry.addTextChangedListener(inputWatcher);
+    final Pattern pLeftInput = Pattern.compile("[0-9]");
 
-
-
-    private void setupUIViews() {
-        left0 = (Button)findViewById(R.id.left0);
-        left1 = (Button)findViewById(R.id.left1);
-        left2 = (Button)findViewById(R.id.left2);
-        left3 = (Button)findViewById(R.id.left3);
-        left4 = (Button)findViewById(R.id.left4);
-        left5 = (Button)findViewById(R.id.left5);
-        left6 = (Button)findViewById(R.id.left6);
-        left7 = (Button)findViewById(R.id.left7);
-        left8 = (Button)findViewById(R.id.left8);
-        left9 = (Button)findViewById(R.id.left9);
-        leftComma = (Button)findViewById(R.id.leftComma);
-        leftDelete = (Button)findViewById(R.id.leftDelete);
-        leftTextEntry = (TextView)findViewById(R.id.leftTextEntry);
-        leftAthlete = (TextView)findViewById(R.id.leftAthlete);
-        leftTrialist = (Button)findViewById(R.id.leftTrialist);
-        leftNoRunner = (Button)findViewById(R.id.leftNoRunner);
 
     }
 
-}
+    private TextWatcher inputWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String leftInput = leftTextEntry.getText().toString();
+
+            if (leftInput.equals("0")) {
+
+                //leftAthlete.setText(("Zero"));
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            try {
+                String leftInput = leftTextEntry.getText().toString();
+                int input = leftTextEntry.length();
+
+                //Normal Entry
+                if (leftInput.length() > 3 && !leftInput.contains(",") && !leftInput.startsWith("0")) {
+                    leftTextEntry.setText(leftInput.substring(0, input - 1));
+                }
+                if (leftInput.length() > 7 && leftInput.contains(",") && !leftInput.startsWith("0") && !leftInput.contains(",0")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+                if (leftInput.length() > 8 && !leftInput.startsWith(("0")) && leftInput.contains(",0")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+
+
+                // Runner is From Another Club
+                if (leftInput.length() > 4 && leftInput.substring(0, 1).equals("0") && !leftInput.contains(",")) {
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+                if (leftInput.length() > 9 && leftInput.startsWith(("0")) && leftInput.contains(",0") ){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+                if (leftInput.length() > 8 && leftInput.startsWith(("0")) && !leftInput.contains(",0") ){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+
+
+
+
+                // If Trialist, Next input has to be comma.
+                if (leftInput.equals("T")) {
+                    leftAthlete.setText("Trialist");
+                }
+                if (leftInput.length() > 5 && leftInput.contains("T,") && !leftInput.contains(",0")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+                if (leftInput.length() > 6 && leftInput.contains("T,") && leftInput.contains(",0")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+                if (leftInput.startsWith("T") && leftInput.length() > 1 && !leftInput.contains(",")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+
+                // Double input on accident
+                if (leftInput.contains(",,") || leftInput.contains("TT") || leftInput.contains("--") || leftInput.contains("T-") || leftInput.contains("-T")|| leftInput.contains("T,") || leftInput.contains(",T") || leftInput.contains(",-")|| leftInput.contains("-,")){
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                }
+
+
+                // If there is no runner
+                if (leftInput.startsWith("-") && leftInput.length() > 1) {
+
+                    leftTextEntry.setText(leftInput.substring(0, input-1));
+                    }
+
+
+            } catch (NumberFormatException e) {
+            }
+        }
+    };
+
+
+            private void setupUIViews () {
+                left0 = (Button) findViewById(R.id.left0);
+                left1 = (Button) findViewById(R.id.left1);
+                left2 = (Button) findViewById(R.id.left2);
+                left3 = (Button) findViewById(R.id.left3);
+                left4 = (Button) findViewById(R.id.left4);
+                left5 = (Button) findViewById(R.id.left5);
+                left6 = (Button) findViewById(R.id.left6);
+                left7 = (Button) findViewById(R.id.left7);
+                left8 = (Button) findViewById(R.id.left8);
+                left9 = (Button) findViewById(R.id.left9);
+                leftComma = (Button) findViewById(R.id.leftComma);
+                leftDelete = (Button) findViewById(R.id.leftDelete);
+                leftTextEntry = (TextView) findViewById(R.id.leftTextEntry);
+                leftAthlete = (TextView) findViewById(R.id.leftAthlete);
+                leftTrialist = (Button) findViewById(R.id.leftTrialist);
+                leftNoRunner = (Button) findViewById(R.id.leftNoRunner);
+
+            }
+        }
+
