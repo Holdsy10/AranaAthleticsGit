39f333350e43dadd8eb9960908fbcf7a6fe7f56c
n8588244 <n8588244@qut.edu.au>
2019-04-30 00:46:30 +1000

    Added Fetch Button to Retrieve URL binary info

--- app/src/main/java/com/example/curtisholdsworth1/aranaathletics1/SignIn.java
+++ app/src/main/java/com/example/curtisholdsworth1/aranaathletics1/SignIn.java
@@ -2,6 +2,7 @@ package com.example.curtisholdsworth1.aranaathletics1;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,14 +14,36 @@ import android.widget.Button;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class SignIn extends AppCompatActivity {
 
@@ -31,12 +54,12 @@ public class SignIn extends AppCompatActivity {
     private ListView listView;
     private LanedEvents tests;
     static List<AthleteSample> athleteSamples = new ArrayList<>();
-
-    private static final String[] NAMES = new String[] {
-            "Curtis","Nathan"
+    byte[] HTML;
+    private static final String FILE_NAME = "fetch2.zip";
+    private static int BUFFER_SIZE = 6 * 1024;
+    private static final String[] NAMES = new String[]{
+            "Curtis", "Nathan"
     };
-
-
 
 
     public List<AthleteSample> getList() {
@@ -69,7 +92,7 @@ public class SignIn extends AppCompatActivity {
 
 
             }
-        } catch(IOException e){
+        } catch (IOException e) {
             Log.wtf("MyActivity", "Error Reading Data File on Line " + line, e);
             e.printStackTrace();
         }
@@ -82,6 +105,7 @@ public class SignIn extends AppCompatActivity {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
+
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             String parentInput = parentsName.getText().toString().trim();
@@ -89,11 +113,11 @@ public class SignIn extends AppCompatActivity {
             unlanedEvents.setEnabled(!parentInput.isEmpty());
             lanedEvents.setEnabled(!parentInput.isEmpty());
         }
+
         @Override
         public void afterTextChanged(Editable s) {
         }
     };
-
 
 
     @Override
@@ -104,14 +128,14 @@ public class SignIn extends AppCompatActivity {
 
         Button goToLaned = (Button) findViewById(R.id.btnlaned);
         Button gotoUnlaned = (Button) findViewById(R.id.btnUnlaned);
+        Button fetchData = (Button) findViewById(R.id.fetch);
         parentsName = findViewById(R.id.autoParentName);
         parentsName.addTextChangedListener(parentsTextWatcher);
 
         lanedEvents = findViewById(R.id.btnlaned);
         unlanedEvents = findViewById(R.id.btnUnlaned);
 
-        readAthletesData();
-
+        //readAthletesData();
 
 
         goToLaned.setOnClickListener(new View.OnClickListener() {
@@ -126,7 +150,7 @@ public class SignIn extends AppCompatActivity {
         gotoUnlaned.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent UnlanedEvents = new Intent (SignIn.this, UnlanedEvents.class);
+                Intent UnlanedEvents = new Intent(SignIn.this, UnlanedEvents.class);
                 startActivity(UnlanedEvents);
             }
         });
@@ -134,8 +158,134 @@ public class SignIn extends AppCompatActivity {
         AutoCompleteTextView editText = findViewById(R.id.autoParentName);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, NAMES);
         editText.setAdapter(adapter);
+
+        /*fetchData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ion.with(getApplicationContext()).load("https://www.aranala.com.au/ipad/fetch2.php").asString()
+                        .setCallback(new FutureCallback<String>() {
+                            @Override
+                            public void onCompleted(Exception e, String result) {
+                                HTML = result.getBytes();
+                                toastMessage("Arana HTML Captured");
+                                Log.d("Debug", HTML.toString());
+
+                                //Save to File After Captured
+                                //save(HTML.toString());
+
+                                //Extract Zip File
+                                try {
+                                    unzip(Environment.getExternalStorageDirectory().getPath(), getFilesDir().toString());
+                                    Log.d("Debug", "Unzip Successfully to" + getFilesDir().toString());
+                                } catch (IOException e1) {
+                                    e1.printStackTrace();
+                                }
+                            }
+                        });
+            }
+        });
+    }*/
+        fetchData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
     }
 
+                //Easy method to write a message to device screen.
+    public void toastMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void save(String text) {
+
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            toastMessage("Saved to " + getFilesDir() + "/" + FILE_NAME);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    /**
+     * Unzip a zip file.  Will overwrite existing files.
+     *
+     * @param zipFile  Full path of the zip file you'd like to unzip.
+     * @param location Full path of the directory you'd like to unzip to (will be created if it doesn't exist).
+     * @throws IOException
+     */
+    public static void unzip(String zipFile, String location) throws IOException {
+        int size;
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        try {
+            if (!location.endsWith(File.separator)) {
+                location += File.separator;
+            }
+            File f = new File(location);
+            if (!f.isDirectory()) {
+                f.mkdirs();
+            }
+            ZipInputStream zin = new ZipInputStream(new BufferedInputStream(new FileInputStream(zipFile), BUFFER_SIZE));
+            try {
+                ZipEntry ze = null;
+                while ((ze = zin.getNextEntry()) != null) {
+                    String path = location + ze.getName();
+                    File unzipFile = new File(path);
+
+                    if (ze.isDirectory()) {
+                        if (!unzipFile.isDirectory()) {
+                            unzipFile.mkdirs();
+                        }
+                    } else {
+                        // check for and create parent directories if they don't exist
+                        File parentDir = unzipFile.getParentFile();
+                        if (null != parentDir) {
+                            if (!parentDir.isDirectory()) {
+                                parentDir.mkdirs();
+                            }
+                        }
+
+                        // unzip the file
+                        FileOutputStream out = new FileOutputStream(unzipFile, false);
+                        BufferedOutputStream fout = new BufferedOutputStream(out, BUFFER_SIZE);
+                        try {
+                            while ((size = zin.read(buffer, 0, BUFFER_SIZE)) != -1) {
+                                fout.write(buffer, 0, size);
+                            }
+
+                            zin.closeEntry();
+                        } finally {
+                            fout.flush();
+                            fout.close();
+                        }
+                    }
+                }
+            } finally {
+                zin.close();
+            }
+        } catch (Exception e) {
+            Log.e("Error", "Unzip exception", e);
+        }
+    }
 
 
 }
